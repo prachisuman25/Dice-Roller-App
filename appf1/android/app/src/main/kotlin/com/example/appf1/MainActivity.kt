ca87cf4f03d2fad173080e8e1f0e9f1c7b86ffc3
slothracing <sumanprachi26@gmail.com>
2023-06-23 01:08:37 +0530

    dice roller app

---
+++ appf1/android/app/src/main/kotlin/com/example/appf1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.appf1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ appf1/lib/gradient_coontainer.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:appf1/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+// ignore: must_be_immutable
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//   final List<Color> colors;
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration:  BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyleText('Prachi123'),
+//       ),
+//     );
+//   }
+// }
